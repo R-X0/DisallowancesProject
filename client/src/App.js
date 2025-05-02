c7e0b41369998b702e13df2cd5672b2fa9f1b323
R-X0 <romilgorgis@outlook.com>
2025-05-02 00:58:44 -0700

    files removed

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ERCProtestForm from './components/ERCProtestForm';
 import AdminDashboard from './components/AdminDashboard';
-import QueueDisplay from './components/QueueDisplay';
-import { Box, Container, Grid } from '@mui/material';
+import { Container } from '@mui/material';
 
-// Updated App component with right-side queue
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={
-          <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={9}>
-                <ERCProtestForm />
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <Box sx={{ position: { md: 'sticky' }, top: { md: '20px' }, height: { md: 'calc(100vh - 40px)' } }}>
-                  <QueueDisplay />
-                </Box>
-              </Grid>
-            </Grid>
+          <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
+            <ERCProtestForm />
           </Container>
         } />
         <Route path="/admin" element={<AdminDashboard />} />
